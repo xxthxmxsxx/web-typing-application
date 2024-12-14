5a57cf33e7c8e1f3be5894347983de0863fad86d
xxthxmxsxx25 <thomasbryant1313@gmail.com>
2024-12-14 17:59:39 +0000

    word recolouring

--- Mainpage/Typingtest.js
+++ Mainpage/Typingtest.js
@@ -1,14 +1,33 @@
 var wordindex = 0;
-var incorrect = 0;
-var correct = 0;
+var correct = 0; 
 
 const input = document.getElementById("enterBox");
 input.addEventListener("keydown",textBoxPress);
-function textBoxPress(x) {
-    const word = document.getElementById(wordindex);
-    console.log(word.textContent)
-    if (x.key == " " && wordindex != 29){
-        wordindex ++;
-    }
+document.getElementById(wordindex).style.color = "rgb(0, 0, 0)"
 
+function wordRecolour(wordEntered, word){
+    if (wordEntered == word.textContent){
+        correct ++;
+        document.getElementById(wordindex).style.color = "rgb(30, 255, 0)";
+    } else {
+        document.getElementById(wordindex).style.color = "rgb(255, 0, 0)";
+    }
+}
+
+function textBoxPress(x) {
+    if (wordindex == 30) {
+        alert("Typing test ended");
+        return;
+    }
+    const word = document.getElementById(wordindex);
+    var wordEntered = document.getElementById("enterBox").value.trim();
+    if (x.key == " ") {
+        wordRecolour(wordEntered, word);
+        document.getElementById("enterBox").value = "";
+        wordindex ++;
+        if (wordindex != 30){
+            document.getElementById(wordindex).style.color = "rgb(0, 0, 0)";
+        }
+        
+    } 
 }
